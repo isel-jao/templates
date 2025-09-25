ede130aab5f10fab3978bae02fead1497070c1c5
issam eljaouhary <iseljao89@gmail.com>
2025-09-25 09:44:53 +0100

    chore: update dependencies and configuration files for improved performance and stability

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,28 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    obfuscatorPlugin({
+      // options passed to javascript-obfuscator
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        debugProtection: false,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: "hexadecimal",
+        stringArrayEncoding: ["base64"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
