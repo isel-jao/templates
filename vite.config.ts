23a21b875e5becefc8410601a02aa7b17c736d8f
issam eljaouhary <iseljao89@gmail.com>
2026-02-03 18:11:39 +0100

    feat: enable obfuscator plugin in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-// import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
-
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,18 +11,17 @@ export default defineConfig({
     react(),
     tailwindcss(),
     svgr(),
-    // obfuscatorPlugin({
-    //   include: ["src/path/to/file.js", "path/anyjs/**/*.js", /foo.js$/],
-    //   exclude: [/node_modules/],
-    //   apply: "build",
-    //   debugger: true,
-    //   options: {
-    //     /** your javascript-obfuscator options */
-    //     // compact: true,
-    //     // controlFlowFlattening: false,
-    //     // ...  [See more options](https://github.com/javascript-obfuscator/javascript-obfuscator)
-    //   },
-    // }),
+    obfuscatorPlugin({
+      include: ["src/path/to/file.js", "path/anyjs/**/*.js", /foo.js$/],
+      exclude: [/node_modules/],
+      apply: "build",
+      debugger: false,
+      options: {
+        compact: true,
+        // controlFlowFlattening: false,
+        // ...  [See more options](https://github.com/javascript-obfuscator/javascript-obfuscator)
+      },
+    }),
   ],
   resolve: {
     alias: {
